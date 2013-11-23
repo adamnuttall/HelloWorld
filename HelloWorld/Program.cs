e1bd7f3e2391f8c7cee09869ad0d38796ee910f0
Adam Nuttall <adam@adamnuttall.co.uk>
2013-11-23 13:21:19 +0000

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(CreateMessage());
+    }
+
+    public static string CreateMessage()
+    {
+      return "Hello World";
+    }
+  }
+}
